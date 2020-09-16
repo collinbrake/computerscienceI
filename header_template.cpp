98512687d228ff3291c8447830b4061e82ac8eb1
Collin Brake <collinmbrake@gmail.com>
2020-09-16 11:03:57 -0400

    added template program with correct header format

---
+++ header_template.cpp
@@ -0,0 +1,8 @@
+/*
+    COPYRIGHT (C) 2017 Collin Brake (cmb361)
+    All rights reserved.
+    <ASSIGNMENT NAME>
+    Author: Collin Brake
+        cmb361@zips.uakron.edu
+    Purpose: <PURPOSE DESCRIPTION>
+*/
