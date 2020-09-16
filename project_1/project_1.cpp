bcc18045b71e7cc2ae2b15107c989bce282db9d0
Collin Brake <collinmbrake@gmail.com>
2020-09-15 21:01:33 -0400

    start of project one (calculating lethal dosage of caffeine)

---
+++ project_1/project_1.cpp
@@ -0,0 +1,18 @@
+/*
+  Pgm: Project 1
+  Name: Collin Brake
+  Purpose: This program calculates the safe amount of caffeine for a person
+  to drink, in cans of caffeine. It computes this amount based on user inputs
+  that provide the weight of the person drinking the caffeine, the weight
+  of a laboratory mouse, and the lethal dose of caffeine for the laboratory mouse.
+  Copyright 2020 by Collin Brake.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
